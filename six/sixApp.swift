3daffe21290a0867f778c0be82a0c45f348f2a9e
Artem <artem@deffun.io>
2026-08-25 04:16:19 +0700

    Initial Commit

---
+++ six/sixApp.swift
@@ -0,0 +1,17 @@
+//
+//  sixApp.swift
+//  six
+//
+//  Created by Артем on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct sixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
